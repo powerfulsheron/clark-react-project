1bd71ae1d412fb21266bdc54f3a97bd2ce8c23b1
Sylvain Coutrot <s.coutrot.ext_azu@sodexo.com>
2019-03-21 23:27:11 +0100

    add loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import clark from './img/Logo.svg';
-import profil from './img/profil.png';
-import deco from './img/3-layers.png';
 import './App.css';
-import ArticleList from './components/ArticleList';
 import SearchPage from './components/SearchPage';
 
 class App extends React.Component {
@@ -16,7 +12,6 @@ class App extends React.Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <br />
-                <SearchPage/>
             </div>
         );
 
--- src/components/SuperLoader/index.js
+++ src/components/SuperLoader/index.js
@@ -4,14 +4,33 @@ import './style/stylesheet-superman.css';
 class SuperLoader extends React.Component {
 
     render() {
+        const styleImage =
+            {
+                margin: 'auto',
+                marginTop: '25vh',
+                marginLeft: '25vw',
+                width: '50vw',
+            };
+        const styleH1 = {
+            position: "absolute",
+        fontWeight: "300",
+        color:"#494949",
+        fontSize: "20px",
+        /*text-transform: uppercase;*/
+        left: "47%",
+        top: "70%",
+        marginLeft:"-20px",
+        fontFamily: "'Lato', sans-serif",
+        }
+
         return(
           <>
               <div className="longfazers">
 
                   <span className=""></span>
-                  <img src={require("./style/supergif.gif")} alt="" />
+                  <img style={styleImage} src={require("./style/supergif.gif")} alt="" />
 
-                  <h1>Clark accède à votre requête...</h1>
+                  <h1 style={styleH1}>Clark accède à votre requête...</h1>
 
               </div>
           </>
