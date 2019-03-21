ff345c6e0303e62a6d75d5f37b873527a41a1527
Nakib <kingamet91@gmail.com>
2019-03-21 20:44:25 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import clark from './img/Logo.png';
+import clark from './img/Logo.svg';
+import profil from './img/profil.png';
+import deco from './img/3-layers.png';
 import './App.css';
 import './style/recherche.css';
 import axios from 'axios';
-import * as d3 from 'd3';
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col'
+
+
 const data = [
     { label: 'CRM', value: 1,  color: '#ff00ff' },
     { label: 'API', value: 1 },
@@ -35,22 +38,24 @@ class App extends Component {
   getArticles = url => {
     axios.get(url).then(response => console.log(response)/* this.setState({ngrams: this.processArticlesToNgrams(articles)})*/);
     };
-
-    ngramNumber = (valuengrams) => {
-        this.setState({
-            value: valuengrams,
-        });
-        console.log(valuengrams);
+    bubbleClick = (label) =>{
+        console.log("Custom bubble click func")
     }
-    handlechange = (event) => {
-        this.setState({
-            query: event.target.value
-        });
-    }
+    // ngramNumber = (valuengrams) => {
+    //     this.setState({
+    //         value: valuengrams,
+    //     });
+    //     console.log(valuengrams);
+    // }
+    // handlechange = (event) => {
+    //     this.setState({
+    //         query: event.target.value
+    //     });
+    // }
   componentDidMount() {
-    this.getArticles('https://api.ozae.com/gnw/articles?date=20180701__20180702&edition=fr-fr&key=11116dbf000000000000960d2228e999&hard_limit=50&topic=b&order[col]=social_score');
+    this.getArticles('https://api.ozae.com/gnw/articles?date=20160601__20180630&key=11116dbf000000000000960d2228e999&query=PSG&hard_limit=20');
     this.setState({ ngrams: data});
-    this.setState({valuengrams: 15});
+    //this.setState({valuengrams: 15});
   }
 
   componentDidUpdate() {
@@ -75,113 +80,125 @@ class App extends Component {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <br />
+            {/*<header className="App-header">*/}
+                {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                {/*<h1 className="App-title">Welcome to React</h1>*/}
+            {/*</header>*/}
+            {/*<br />*/}
             {/*<div>*/}
                 {/*{this.state.ngrams.map((ngram) => <p>{ngram.label}</p>)}*/}
             {/*</div>*/}
+            <div className="col-left">
+                <img src={deco} alt="Clark" className="deco"/>
+                <div className="deconnexion">Deconnexion</div>
+                <>
+                <input className="input-live"type="text" onChange={this.handlechange} />
+            </>
+                <div className="Bubble">
+                    <BubbleChart
+                        graph= {{
+                            zoom: 0.7,
+                            offsetX: -0.04,
+                            offsetY: -0.01,
+                        }}
+                        width={window.innerWidth=800}
+                        height={window.innerHeight=800}
+                        fontFamily="Lato"
+                        showLegend={false}
+                        data={this.state.ngrams}
+                        labelFont={{
+                            family: 'Lato',
+                            size: 16,
+                            color: '#fff',
+                            weight: 'bold',
+                        }}
 
-                    <img src={clark} alt="Clark" className="header-logo"/>
+                    />
+            </div>
+
+            </div>
+            <div className="sidebar">
+
+                <div>
+                    <img src={profil} alt="Clark" className="header-logo"/>
+                    {/*<p className="compte">Mon compte</p>*/}
+                </div>
+                <div className="ngrams-choice">
+                <div id="ngrams">15</div>
+                    <p className="keys">Keywords</p>
+                <>
+                    <input type="range" className="ngram-number" onChange={this.ngramNumber} max="30" min="15"
+                           step="1"/>
+                </>
+            </div>
+
+                <div className="strategy">
+
+                    <h1 id="strategy">Strategy</h1>
+                    <hr/>
+                    <ul>
+                        <li><label className="container">
+                            <input type="radio" name="choice" checked="checked"/>
+                            Social Media
+                        </label>
+                        </li>
+                        <li>
+                            <label className="container">
+                                <input type="radio" name="choice"/>
+                                Buzz
+                            </label>
+                        </li>
+                        <li>
+                            <label className="container">
+                                <input type="radio" name="choice"/>
+                                SEO
+                            </label>
+                        </li>
+                    </ul>
+                    <div className="parameters">
+                        <p>Advenced parameters…</p>
+                        <hr/>
                         <>
-                        <input className="input-live"type="text" onChange={this.handlechange} />
-                    </>
-
-
-                        <div className="ngrams-choice">
-                        <div id="ngrams">15</div>
-                        <>
-                            <input type="range" className="ngram-number" onChange={this.ngramNumber} max="30" min="15"
-                                   step="1"/>
-                        </>
-                        </div>
-            <div className="Bubble">
-                            <BubbleChart
-                                graph= {{
-                                    zoom: 1.1,
-                                    offsetX: -0.05,
-                                    offsetY: -0.01,
-                                }}
-                                width={window.innerWidth=800}
-                                height={window.innerHeight=800}
-                                fontFamily="Leto"
-                                showLegend={false}
-                                data={this.state.ngrams}
-                                labelFont={{
-                                    family: 'Leto',
-                                    size: 16,
-                                    color: '#fff',
-                                    weight: 'bold',
-                                }}
-
-                            />
-
-                        <div className="strategy">
-
-                            <h1>Strategy</h1>
-                            <hr/>
                             <ul>
-                                <li><label className="container">
-                                    <input type="radio" name="choice" checked="checked"/>
-                                    Social Media
-                                </label>
-                                </li>
                                 <li>
-                                    <label className="container">
-                                        <input type="radio" name="choice"/>
-                                        Buzz
+                                    <label>Engagement
+                                        <input type="range" className="range1 engagement"  max="5" min="1"
+                                               step="1"/>
                                     </label>
                                 </li>
                                 <li>
-                                    <label className="container">
-                                        <input type="radio" name="choice"/>
-                                        SEO
+                                    <label>Progation
+                                        <input type="range" className="range2"  max="5" min="1"
+                                               step="1"/>
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>Densité
+                                        <input type="range" className="range3"  max="5" min="1"
+                                               step="1"/>
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>Fiabilité
+                                        <input type="range" className="range4"  max="5" min="1"
+                                               step="1"/>
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>Position
+                                        <input type="range" className="range5"  max="5" min="1"
+                                               step="1"/>
                                     </label>
                                 </li>
                             </ul>
-                            <div className="parameters">
-                                <p>Advenced parameters…</p>
-                                <hr/>
-                                <>
-                                    <ul>
-                                        <li>
-                                    <label>Engagement
-                                    <input type="range" className="range1 engagement"  max="5" min="1"
-                                           step="1"/>
-                                    </label>
-                                        </li>
-                                        <li>
-                                    <label>Progation
-                                    <input type="range" className="range2"  max="5" min="1"
-                                           step="1"/>
-                                    </label>
-                                        </li>
-                                        <li>
-                                    <label>Densité
-                                    <input type="range" className="range3"  max="5" min="1"
-                                           step="1"/>
-                                    </label>
-                                        </li>
-                                        <li>
-                                    <label>Fiabilité
-                                    <input type="range" className="range4"  max="5" min="1"
-                                           step="1"/>
-                                    </label>
-                                        </li>
-                                        <li>
-                                    <label>Position
-                                    <input type="range" className="range5"  max="5" min="1"
-                                           step="1"/>
-                                    </label>
-                                        </li>
-                                    </ul>
-                                </>
-                            </div>
-                        </div>
+                        </>
+                    </div>
+                </div>
             </div>
-        </div>
+
+
+            </div>
+
 
     );
 
