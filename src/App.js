6d8a39659985694c1c891d2344cf29d1142c3125
TqtPasJeCarryLeProj <lorenzo.canavaggio@laposte.net>
2019-03-21 20:55:29 +0100

    landing page near done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ArticleList from './components/ArticleList';
+import clark from './images/Logo.svg';
+import {Container,Row,Col} from 'react-bootstrap';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
 
@@ -11,15 +16,137 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <br />
-            <ArticleList/>
+            <Container>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Grid container spacing={20}>
+                    <Grid item xs={2}>
+                    </Grid>
+                    <Grid item xs={8 }>
+                        <img src={clark} alt="Clark Kent" height="300" width="300"></img>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Button variant="contained" style={{
+                            backgroundColor: "#5CABCC",
+                            color:"white"
+                        }}>
+                            Register
+                        </Button>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Button variant="contained" style={{
+                            backgroundColor: "#44D7B6",
+                            color:"white"
+                        }}>
+                            Login
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Row>
+                    <Col>
+                        <label className="lpLabel">" Your SEO Super Hero "</label>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <label className="lpheader">Générez vos mots clés qualifiés</label>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Grid container spacing={20}>
+                    <Grid item xs>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            id="standard-full-width"
+                            style={{ margin: 8 }}
+                            placeholder="🔍 ex : Trump"
+                            fullWidth
+                            margin="normal"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs>
+                    </Grid>
+                </Grid>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button variant="contained" className="primary" style={{
+                            backgroundColor: "#6995d7",
+                            color:"white"
+                        }}>
+                            Essayez gratuitement
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <br></br>
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Grid container spacing={20}>
+                    <Grid item xs={1}>
+                    </Grid>
+                    <Grid item xs>
+                        <h1 className="lpheader">Interpretation</h1>
+                        <label className="lppara">Des datas sous forme de bubble chart, en fonction de la position d'un mot dans son reférencement</label>
+                    </Grid>
+                    <Grid item xs>
+                    </Grid>
+                    <Grid item xs>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={20}>
+                    <Grid item xs>
+                    </Grid>
+                    <Grid item xs>
+                    </Grid>
+                    <Grid item xs>
+                        <h1 className="lpheader">Customisation</h1>
+                        <label className="lppara">De la Dataviz en fonction de stratégie souhaitée, avec des paramètres avancés</label>
+                    </Grid>
+                    <Grid item xs={1}>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
-
   }
 }
 
